8ff35ebdbd8ad9d1bb774c36dc37f759e2bfc7bb
Hana K <66429579+hanakola@users.noreply.github.com>
2021-01-02 20:38:13 +0000

    Created homepage component, installed Sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Homepage from "./pages/homepage";
 
 import "./App.css";
 
 function App() {
-  return <div>E-commerce</div>;
+  return <Homepage />;
 }
 
 export default App;
