aa44afd5d6bb5bc5914ebc4076642fc2a60483a1
Hana K <66429579+hanakola@users.noreply.github.com>
2021-01-04 17:52:28 +0000

    Added React Router for navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage";
 
 import "./App.css";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
-  return <Homepage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
