a7674186fa46d43238a0a345d06ba3ccb792c7d3
Hana K <66429579+hanakola@users.noreply.github.com>
2021-01-06 14:51:52 +0000

    Created header with logo and navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
