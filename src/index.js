aa5cd016c96e4652a8ce3b34425ebcd6a86e93aa
Hana K <66429579+hanakola@users.noreply.github.com>
2021-01-23 18:19:16 +0000

    Integrated redux-persist to persist a store object in local storage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 
@@ -11,7 +12,9 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
